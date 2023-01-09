c6116577377cede04881ec87296fdad43a034da3
Samuel Raducan <samuel.thembp@gmail.com>
2023-01-09 17:49:36 +0000

    Add first styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import CarValue from './components/CarValue';
 
 const App = () => {
 	return (
-		<div>
+		<div className="container is-fluid">
 			<CarForm />
-			<CarList />
 			<CarSearch />
+			<CarList />
 			<CarValue />
 		</div>
 	);
