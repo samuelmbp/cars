1c85b355e9ff16ceef6a42c46adb80e7cb6417a6
Samuel Raducan <samuel.thembp@gmail.com>
2023-01-09 17:41:50 +0000

    List the car records

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -5,9 +5,30 @@ const CarList = () => {
 		return state.cars.data;
 	});
 
-	console.log(cars);
+	const handleCarDelete = (car) => {};
 
-	return <div>CarList</div>;
+	const renderedCars = cars.map((car) => {
+		return (
+			<div className="panel" key={car.id}>
+				<p>
+					{car.name} - £{car.cost}
+				</p>
+				<button
+					className="button is-danger"
+					onClick={() => handleCarDelete(car)}
+				>
+					Delete
+				</button>
+			</div>
+		);
+	});
+
+	return (
+		<div className="car-list">
+			{renderedCars}
+			<hr />
+		</div>
+	);
 };
 
 export default CarList;
