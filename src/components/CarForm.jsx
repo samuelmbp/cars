7306ca1dd9a615e0d3ab6da14cdf7d9cc5587f6a
Samuel Raducan <samuel.thembp@gmail.com>
2023-01-09 17:16:40 +0000

    Update state for the cost input

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,16 +1,24 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { changeName } from '../store';
+import { changeName, changeCost } from '../store';
 
 const CarForm = () => {
 	const dispatch = useDispatch();
 
-	// Access state from formSlice 
-	const name = useSelector((state) => {
-		return state.form.name;
+	// Access state from formSlice
+	const { name, cost } = useSelector((state) => {
+		return {
+			name: state.form.name,
+			cost: state.form.cost,
+		};
 	});
 
 	const handleNameChange = (event) => {
 		dispatch(changeName(event.target.value));
+	};
+
+	const handleCostChange = (event) => {
+		const carCost = parseInt(event.target.value) || 0;
+		dispatch(changeCost(carCost));
 	};
 
 	return (
@@ -19,14 +27,21 @@ const CarForm = () => {
 			<form action="">
 				<div className="field-group">
 					<div className="field">
-						<label htmlFor="" className="label">
-							Name
-						</label>
+						<label className="label">Name</label>
 						<input
 							className="input is-expanded"
 							type="text"
 							value={name}
 							onChange={handleNameChange}
+						/>
+					</div>
+					<div className="field">
+						<label className="label">Cost</label>
+						<input
+							className="input is-expanded"
+							type="number"
+							value={cost || ''}
+							onChange={handleCostChange}
 						/>
 					</div>
 				</div>
