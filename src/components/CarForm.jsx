dba2335aa9888cfadbf183f4e4005b440f3b083f
Samuel Raducan <samuel.thembp@gmail.com>
2023-01-09 17:23:00 +0000

    Dispatch during the form submission

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { changeName, changeCost } from '../store';
+import { changeName, changeCost, addCar } from '../store';
 
 const CarForm = () => {
 	const dispatch = useDispatch();
@@ -21,10 +21,15 @@ const CarForm = () => {
 		dispatch(changeCost(carCost));
 	};
 
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		dispatch(addCar({ name, cost }));
+	};
+
 	return (
 		<div className="car-form panel">
 			<h4 className="subtitle is-3">Add Car</h4>
-			<form action="">
+			<form onSubmit={handleSubmit}>
 				<div className="field-group">
 					<div className="field">
 						<label className="label">Name</label>
@@ -44,6 +49,9 @@ const CarForm = () => {
 							onChange={handleCostChange}
 						/>
 					</div>
+				</div>
+				<div className="field">
+					<button className="button is-link">Submit</button>
 				</div>
 			</form>
 		</div>
