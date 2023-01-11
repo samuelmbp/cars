0444793ff70e3c0f69ea6d10336713844abb3402
Samuel Raducan <samuel.thembp@gmail.com>
2023-01-11 11:51:23 +0000

    Add searching input

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,5 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchTerm } from '../store';
+
 const CarSearch = () => {
-	return <div>CarSearch</div>;
+	const dispatch = useDispatch();
+
+	const { searchTerm } = useSelector((state) => {
+		return state.cars;
+	});
+
+	console.log(searchTerm);
+
+	const handleSearchTermChange = (event) => {
+		dispatch(changeSearchTerm(event.target.value));
+	};
+
+	return (
+		<div className="list-header">
+			<h3 className="title is-3">My Cars</h3>
+			<div className="search field is-horizontal">
+				<label className="label">Search</label>
+				<input
+					type="text"
+					className="input"
+					value={searchTerm}
+					onChange={handleSearchTermChange}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default CarSearch;
